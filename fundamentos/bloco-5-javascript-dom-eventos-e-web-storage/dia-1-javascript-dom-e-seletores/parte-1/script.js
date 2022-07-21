8f6d0666a7c3c48df594b50ff20e3ac722e884f1
Thales Spanhol <99999595+thspanhol@users.noreply.github.com>
2022-07-21 20:30:26 -0300

    Delete fundamentos directory

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/parte-1/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "pink";
-document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "lightgreen";
-document.getElementsByTagName("h3")[0].style.backgroundColor = "gray";
-document.getElementsByTagName("h3")[1].style.backgroundColor = "gray";
-document.getElementsByTagName("h3")[2].style.backgroundColor = "darkgray";
-document.getElementsByTagName("h3")[3].style.backgroundColor = "darkgray";
